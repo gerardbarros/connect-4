0ec92c747907ed73737c485e8188db4b67cc4e4c
Minh Doan <minhadoan@gmail.com>
2022-05-17 07:49:06 -0500

    Made a grid for the play area and added some colors

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-alert('test')
+// create an event listener for the divs
+
+// when clicked it should create a circle inside with a certain color
+
+//make it so that the colors alternate (perhaps you could use turn counter even/odd)
+
+// the game will run until the winning conditions are met or no moves are left
+
+// populate a message somewhere when a win condition is met or no moves are left for a draw
+
+// the hard part is creating the win condiitons
+// you could use an array or some sort of data grid to assign the div class numbers and create win conditions based off the location
+// connect 4 is magnitudes more complex than ticTacToe due to a large increase in area of the box (from 9 to 42 total)
+// if you could find some logic that is not location based it may be easier
